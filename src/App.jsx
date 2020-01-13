b8f2423525768d0189e2afb3d0ed82a43d418266
Wolgan Ens <wolgan.ens@brasilparalelo.com.br>
2020-01-12 22:27:30 -0300

    add auth user to the app state

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,20 @@ class App extends React.Component{
     }
   }
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user);
-      this.setState({currentUser: user});
-      console.log(this.state);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({currentUser: {
+            id: snapShot.id,
+            ...snapShot.data()
+          }}, () => console.log(this.state));
+        });
+      } else {
+        this.setState({
+          currentUser: userAuth
+        }, () => console.log(this.state));
+      }
     });
   }
 
